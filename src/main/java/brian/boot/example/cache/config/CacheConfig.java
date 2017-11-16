46c74ec75c81e67cad82257a560cd862a567b9f8
allthatjava <allthatjava@yahoo.ca>
2017-11-16 09:21:40 -0500

    Update the comment in CacheConfig class

--- src/main/java/brian/boot/example/cache/config/CacheConfig.java
+++ src/main/java/brian/boot/example/cache/config/CacheConfig.java
@@ -1,12 +1,6 @@
 package brian.boot.example.cache.config;
 
-import java.util.Arrays;
-
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCache;
-import org.springframework.cache.support.SimpleCacheManager;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
@@ -14,16 +8,16 @@ import org.springframework.context.annotation.Configuration;
 public class CacheConfig {
 	
 // TODO : This was not needed since applicatoin.yml has spring.cache.type as 'simple'.
-//			When you have multiple cache configuration such as having different caching service by Profile,
-//				CacheManager bean must be added for different environments.
-    @Bean
-    public CacheManager cacheManager() {
-        SimpleCacheManager cacheManager = new SimpleCacheManager();
-        cacheManager.setCaches(
-        		Arrays.asList(
-        			new ConcurrentMapCache("post-single") 
-        		)
-        );
-        return cacheManager;
-    }
+//			When you have multiple cache configuration for different environments,
+//				CacheManager bean must be added for each different environments(Profile).
+//    @Bean
+//    public CacheManager cacheManager() {
+//        SimpleCacheManager cacheManager = new SimpleCacheManager();
+//        cacheManager.setCaches(
+//        		Arrays.asList(
+//        			new ConcurrentMapCache("post-single") 
+//        		)
+//        );
+//        return cacheManager;
+//    }
 }
